cf2c2630a8ecfc78364f8752be839747200d2f55
paku <paku@skyizwhite.dev>
2024-07-25 16:23:00 +0900

    Deploy web server cluster

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "public_id" {
-  value       = aws_instance.example.public_ip
-  description = "The pubic IP address of the web server"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "server_port" {
   type        = number
   default     = 8080
 }
+
+variable "alb_name" {
+  description = "The name of the ALB"
+  type        = string
+  default     = "tofu-asg-example"
+}
+
+variable "instance_security_group_name" {
+  description = "The name of the security group for the EC2 Instances"
+  type        = string
+  default     = "tofu-example-instance"
+}
+
+variable "alb_security_group_name" {
+  description = "The name of the security group for the ALB"
+  type        = string
+  default     = "tofu-example-alb"
+}
